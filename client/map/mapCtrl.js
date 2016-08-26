49da10ca67c2121f63bc240d2c3a7ad359d85191
yurik1776@gmail.com <yurik1776@gmail.com>
2016-08-26 05:13:52 +0300

    fixed markers and polilines on changes, need to work with popups

--- client/map/mapCtrl.js
+++ client/map/mapCtrl.js
@@ -6,49 +6,39 @@
             var mymap = L.map('leafmap').setView([32.32, 34.86], 13);
             var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mymap);
             var markers = [];
-            var marker;
-            var poli_counter = 1;
-            var poli;
-            var stop_counter = 1;
+            var poli_count = 1;
+            var polilines = [];
+            var full = [];
             mymap.on('click', function(e){
+
                 var marker = L.marker(e.latlng,{draggable:true});
 
-                    marker.bindPopup('<div>Stop number ' + stop_counter + '</div>' + '<input type="text" placeholder="add your stop here" id="message"  />')
+                    marker.bindPopup('<input type="text" placeholder="add your stop here" id="message"/>')
                     marker.addTo(mymap)
-
-                    markers.push(marker);
-
-
 
                     marker.on('click', function(e){
                         mymap.panTo(e.latlng);
                         marker.openPopup();
                         var input = L.DomUtil.get('message');
+
                         input.addEventListener('keydown',function(e){
                             if(e.keyCode === 13){
-
-                                marker.bindPopup('<div>Stop number ' + stop_counter + '</div>' +  e.target.value);
-                                stop_counter++;
+                                marker.bindPopup(e.target.value);
+                                markers.push(marker._latlng);
+                                full.push(marker);
                                 if(markers.length>1){
-                                    var latlngs = [];
+                                    if(poli_count === 1){
+                                        var poli = L.polyline([markers[poli_count-1],markers[poli_count]],{
+                                            color:'red'
+                                        })
+                                    }else{
+                                        var poli = L.polyline([markers[poli_count-1],markers[poli_count]])
+                                    }
 
-
-                                    latlngs.push(new L.LatLng(markers[poli_counter-1]._latlng.lat,markers[poli_counter-1]._latlng.lng))
-                                    latlngs.push(new L.LatLng(markers[poli_counter]._latlng.lat,markers[poli_counter]._latlng.lng))
-                                    poli_counter++;
-
-                                    poli = L.polyline(latlngs,
-                                    {
-                                        color: 'red',
-                                        weight: 3,
-                                        opacity: 0.5,
-                                        smoothFactor: 1
-                                    });
                                     poli.addTo(mymap);
-
-
+                                    polilines.push(poli);
+                                    poli_count++;
                                 }
-
                                 marker.off('click');
                                 marker.on('click', function(e){
                                     mymap.panTo(e.latlng)
@@ -56,22 +46,40 @@
 
                                 })
                             }
-
                         });
-
                     });
+
                     marker.on('contextmenu', function(e){
-                        mymap.removeLayer(marker);
-                        var index = markers.indexOf(marker);
+                        mymap.removeLayer(marker)
+                        var index = markers.indexOf(marker._latlng);
                         markers.splice(index, 1);
-                        mymap.removeLayer(poli);
-                        console.log(markers)
-                        poli_counter--;
-                        stop_counter--;
+                        var second_index = full.indexOf(marker);
+                        full.splice(second_index, 1);
+                        mymap.eachLayer(function(layer){
+                            mymap.removeLayer(layer);
+                        })
+                        var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mymap);
+                        for(var i=1,len=markers.length;i<len;i++){
+                            if(i === 1){
+                                var newPoli = L.polyline([markers[i-1],markers[i]],{color:'red'});
+                            }else{
+                                var newPoli = L.polyline([markers[i-1],markers[i]]);
+                            }
+
+                            newPoli.addTo(mymap);
+
+                        }
+                        for(var i=0,len=markers.length;i<len;i++){
+                            full[i].addTo(mymap)
+                        }
+                        if(markers.length === 0 ){
+                            poli_count = 1;
+                        }else{
+                            poli_count = markers.length;
+                        }
 
 
                     })
                 })
-
         }])
 })()
